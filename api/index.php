1bcae739cc9a94089d7c20644b883e2710ec7975
Aurelio John Bautista <nox.elle.412@gmail.com>
2026-06-01 15:44:05 +0800

    fix: force storage memory layout in api entrypoint

--- api/index.php
+++ api/index.php
@@ -1,2 +1,7 @@
 <?php
+
+// Force the configuration and view cache paths to live in Vercel's writable memory space
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+
 require __DIR__ . '/../public/index.php';
