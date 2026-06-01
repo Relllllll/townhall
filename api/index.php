45d27dadcbd7ad830734c3e9053850527f7e5f01
Aurelio John Bautista <nox.elle.412@gmail.com>
2026-06-01 15:54:28 +0800

    fix: re-align routing definitions

--- api/index.php
+++ api/index.php
@@ -1,6 +1,9 @@
 <?php
 
-// 1. Force the serverless memory layout to stop read-only crashes
+// Temporary test marker to prove Vercel is running this file
+// if you see this in your browser, it means the routing works!
+// echo "Vercel is running our custom entrypoint!"; die();
+
 $_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp';
 $_ENV['CACHE_STORE'] = 'array';
@@ -8,9 +11,7 @@ $_ENV['CACHE_DRIVER'] = 'array';
 $_ENV['LOG_CHANNEL'] = 'stderr';
 $_ENV['SESSION_DRIVER'] = 'cookie';
 
-// 2. Automatically parse your Vercel/Neon URL and hand it to Laravel
 $dbUrl = getenv('POSTGRES_URL') ?: getenv('DATABASE_URL');
-
 if ($dbUrl) {
     $dbParts = parse_url($dbUrl);
     $_ENV['DB_CONNECTION'] = 'pgsql';
